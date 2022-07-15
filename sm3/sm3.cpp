475f6e4abc905b331cd3939619e3a05b23c47bad
wfq0007 <105547875+wfq0007@users.noreply.github.com>
2022-07-16 00:25:33 +0800

    Add files via upload

---
+++ sm3/sm3.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include "sm3.h"
+using namespace std;
+
+void printt_u8(u8* input);
+
+int main()
+{
+	u8 input[256] = "abc";
+	//u8 input[256] = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd";//输入
+	u8 output[32];//256杂凑值输出
+	unsigned int len = 3;
+	SM3(input, len, output);
+	cout << "哈希值为：" << endl;
+	printt_u8(output);
+	return 0;
+}
+
+void printt_u8(u8* output) {
+	for (int i = 0; i < 32; i++)//16进制输出
+	{
+		int a = output[i];
+		if (a < 16)
+			cout << "0x0" << hex << a << " ";
+		else
+			cout << "0x" << hex << a << " ";
+		if ((i + 1) % 8 == 0)
+			cout << endl;
+	}
+	cout << endl;
+}
