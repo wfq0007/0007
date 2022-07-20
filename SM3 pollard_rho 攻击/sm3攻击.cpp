e0abeb87e584b54c5195004c3e11bcb65412c6ac
wfq0007 <105547875+wfq0007@users.noreply.github.com>
2022-07-20 12:12:31 +0800

    Add files via upload

---
+++ SM3 pollard_rho 攻击/sm3攻击.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include "sm3.h"
+using namespace std;
+
+void printt_u8(u8* input);
+
+int main()
+{
+	//存储简化的pollard_rho算法。
+	u8 input[33] = "a";
+	unsigned int len = 32;
+	u8 output1[32];
+	u8 output2[32];
+	SM3(input, 1, output1);
+	cout << "步长为1：";
+	printt_u8(output1);
+	SM3(output1, 32, output2);
+	cout << "步长为2：";
+	printt_u8(output2);
+	unsigned int num = 0;
+	while (1)
+	{
+		num++;
+		//cout << "第" << dec << num << "次" << endl;
+		u8 temp1[32];
+		u8 temp2[32];
+		SM3(output1, 32, temp1);
+		for (int i = 0; i < 32; i++) {
+			output1[i] = temp1[i];
+		}
+		//cout << "步长为1：";
+		//printt_u8(output1);
+		SM3(output2, 32, temp2);
+		SM3(temp2, 32, output2);
+		//cout << "步长为2：";
+		//printt_u8(output2);
+		//比较是否相同
+		if (output1 == output2) {
+			cout << "成功";
+			break;
+		}
+		if (num % 65536 == 0) {
+			cout << "第" << dec << num << "次" << endl;
+		}
+	}
+	//运算为hash，一个步长为1，一个步长为2。
+	/*
+	u8 input[256] = "abc";
+	//u8 input[256] = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd";//输入
+	u8 output[32];//256杂凑值输出
+	unsigned int len = 3;
+	SM3(input, len, output);
+	cout << "哈希值为：" << endl;
+	printt_u8(output);
+	*/
+	return 0;
+}
+
+void printt_u8(u8* output) {
+	for (int i = 0; i < 32; i++)//16进制输出
+	{
+		int a = output[i];
+		if (a < 16)
+			cout << "0" << hex << a ;
+		else
+			cout << hex << a ;
+	}
+	cout << endl;
+}
