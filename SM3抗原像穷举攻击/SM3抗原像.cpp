090a8b93b724089b33cef6cc2dbe3859e4e97cd1
wfq0007 <105547875+wfq0007@users.noreply.github.com>
2022-07-25 01:53:48 +0800

    Update SM3抗原像.cpp

--- SM3抗原像穷举攻击/SM3抗原像.cpp
+++ SM3抗原像穷举攻击/SM3抗原像.cpp
@@ -1,7 +1,12 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #include "sm3.h"
 using namespace std;
+
+
+u8 input[256];//先限制在256内
+unsigned int len = 25;
+u8 output[32];//256杂凑值输出
 
 void printt_u8(u8* input);
 int* find_next(string p);//计算模式串的next
@@ -23,25 +28,77 @@ int pp(u8* output) {
 	//不存在返回-1
 }
 
+void search(int pos, int e1, int s2)
+{
+	if (pos == 0) {
+		if (pos == e1) {
+			search(pos + 24, e1, s2);
+		}
+		else {
+			search(pos + 1, e1, s2);
+		}
+	}
+	else if (pos < (len - 1))
+	{
+		for (int i = 0; i < 0xff; i++) {
+			//printt_u8(input);
+			if (pos == e1) {
+				search(pos + 24, e1, s2);
+			}
+			else {
+				search(pos + 1, e1, s2);
+			}
+		}
+	}
+	if (pos == len - 1)
+	{
+		for (int j = 0; j <= 0xff; j++)
+		{
+			SM3(input, len, output);
+			if (pp(output) != -1)
+			{
+				std::cout << "找到了" << endl;
+				printt_u8(output);
+				exit(0);
+			}
+			input[pos]++;
+		}
+		bool ss = false;
+		int tt = 0;
+		while (!ss)
+		{
+			input[pos - tt] = 0x00;
+			tt++;
+			if ((pos - tt) == (s2 - 1)){
+				tt += 24;
+			}
+			if (tt == len) {
+				ss = true;
+			}
+			else if (input[pos - tt] != 0xff) {
+				input[pos - tt]++;
+				ss = true;
+			}
+			//printt_u8(input);
+		}
+	}
+}
 int main()
 {
 	u8 iin[25] = "Fangqi Wang 202000460007";
-	u8 output[32];//256杂凑值输出
 	//u8 input[256] = "abcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcdabcd";//输入
 	//修改输入，以64字节长为基准
 	//输入数据串的长度由24开始依次尝试
 	SM3(iin, 24, output);
 	if (pp(output) != -1)
 	{
-		cout << "找到了" << endl;
+		std::cout << "找到了" << endl;
 		printt_u8(output);
 		exit(0);
 	}
-	unsigned int len = 25;
 	while (1)
 	{
-		u8 input[256];//先限制在256内
-		for (int xx = 0; xx < len - 24; xx++)
+		for (int xx = 0; xx < (len - 24); xx++)
 		{
 			int w = 0;
 			for (int i = xx; i < xx + 24; i++) {
@@ -54,38 +111,16 @@ int main()
 			for (int i = xx + 24; i < len; i++) {
 				input[i] = 0x00;
 			}
-			//cout << xx << "次" << endl;
-			//printt_u8(input);
-			for (int i = 0; i < xx; i++) {
-				for (int j = 0; j < 0xff; j++)
-				{
-					SM3(input, len, output);
-					if (pp(output) != -1)
-					{
-						cout << "找到了" << endl;
-						printt_u8(output);
-						exit(0);
-					}
-					input[i]++;
-				}
-				//cout << i << "  又完成了256次" << endl;
+			search(0, xx, xx + 24);
+			/*
+			if (xx != 0) {
+				search(0, 0, xx);
 			}
-			for (int i = xx + 24; i < len; i++) {
-				for (int j = 0; j < 0xff; j++)
-				{
-					SM3(input, len, output);
-					if (pp(output) != -1)
-					{
-						cout << "找到了" << endl;
-						printt_u8(output);
-						exit(0);
-					}
-					input[i]++;
-				}
-				//cout << i << "  又完成了256次" << endl;
-			}
+			if ((xx + 24) != len) {
+				search(xx + 24, xx + 24, len);
+			}*/
 		}
-		cout << "此时长度为:" << len << endl;
+		std::cout << "此时长度为:" << len << endl;
 		len++;
 	}
 	return 0;
@@ -96,9 +131,9 @@ void printt_u8(u8* output) {
 	{
 		int a = output[i];
 		if (a < 16)
-			cout << "0" << hex << a;
+			std::cout << "0" << hex << a;
 		else
-			cout << hex << a;
+			std::cout << hex << a;
 	}
-	cout << endl;
+	std::cout << endl;
 }
