5c7821c94c8c8ed6802e8b8af06efdd482aabf92
wfq0007 <105547875+wfq0007@users.noreply.github.com>
2022-07-27 19:07:29 +0800

    Add files via upload

---
+++ SM2实现/SM2加解密.py
@@ -0,0 +1,178 @@
+#尝试实现SM2
+#gmssl看看
+import base64
+import binascii
+from random import randint
+from gmssl import sm3,func
+import math
+
+
+#推荐使用素数域256位椭圆曲线
+#ECC参数
+#方程y2=x3+ax+b
+
+p=0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF
+a=0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFC
+b=0x28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93
+n=0xFFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123
+GX=0x32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7
+GY=0xBC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0
+
+
+def gcd(p,q):
+    '''欧几里得算法求最大公因子'''
+    if q>p:
+        p,q=q,p
+    while(q!=0):
+        p,q=q,p%q
+    return p
+
+
+def xcd(p,n):#前提n,p互质
+    '''扩展欧几里得算法求模逆'''
+    s=[1,0]
+    t=[0,1]
+    r=[n,p]
+    rr=n%p
+    while(rr!=0):
+        q=r[0]//r[1]
+        r.append(rr)
+        s.append(s[0]-q*s[1])
+        t.append(t[0]-q*t[1])#边做带余除法边线性表出
+        r.pop(0);s.pop(0);t.pop(0)
+        rr=r[0]%r[1]
+    return t[1]#r=s*n+t*p,t[1]为模逆
+
+
+def addit(x1,y1,x2,y2,a,p):
+    '''两点相加'''
+    if(x1==x2 and y1==p-y2):
+        return false
+    elif(x1==x2):#倍点
+        lmd=(((3*x1*x1+a)%p)*xcd(2*y1,p))%p
+    else:#两个非互逆的不同点
+        lmd=((y2-y1)%p*xcd((x2-x1)%p,p))%p
+    x3=(lmd*lmd-x1-x2)%p
+    y3=(lmd*(x1-x3)-y1)%p
+    return x3,y3
+        
+    
+def multipoint(x,y,k,a,p):
+    '''计算椭圆曲线点'''
+    k=bin(k)[2:]#变成二进制字符串,获得二进制长度
+    xp,yp=x,y
+    #类似平方-乘模幂算法
+    for i in range(1,len(k)):#循环长度减1次
+        xp,yp=addit(xp,yp,xp,yp,a,p)#"平方"
+        if k[i]=='1':
+            xp,yp=addit(xp,yp,x,y,a,p)#"乘"
+    return xp,yp
+
+
+def KDF(st,klen):
+    '''密钥派生函数'''
+    ct=1
+    k=''
+    for i in range(math.ceil(klen/256)):
+        tp=hex(int(st+'{:032b}'.format(ct),2))[2:]
+        tp1=sm3.sm3_hash(func.bytes_to_list(tp.encode()))
+        k+=bin(int(tp1,16))[2:].rjust(256,'0')
+        ct+=1
+    '''
+    #填充
+    if klen/256!=klen//256:
+        tpp=klen-(256*(klen//256))
+        k=k[:(-256+tpp)]
+    '''
+    return k[:klen]
+    
+    
+def encrypt(dB,xB,yB,message):
+    '''加密'''
+    tp=bin(int(message.encode().hex(),16))[2:]
+    tpp=4-(len(tp)%4)#计算填零对齐长度
+    m=tpp*'0'+tp#转为比特串
+    klen=len(m)#明文比特串长度
+    plen=len(hex(p)[2:])
+    while True:
+        k=randint(1,n-1)
+        while(k==dB):
+            k=randint(1,n-1)#k不能正好选到私钥
+        x1,y1=multipoint(GX,GY,k,a,p)
+        x2,y2=multipoint(xB,yB,k,a,p)#计算k[PB]
+        #转为比特串,256位椭圆曲线
+        x2='{:0256b}'.format(x2)
+        y2='{:0256b}'.format(y2)
+        #计算t
+        t=KDF(x2+y2,klen)
+        if t!=0:#直至t不全为0
+            break
+    #点到比特串的转换,未压缩表示形式
+    x1=hex(x1)[2:].rjust(plen,'0')
+    y1=hex(y1)[2:].rjust(plen,'0')
+    C1='04'+x1+y1
+    #如果异或后恰好整字节为0，填充
+    C2=hex(int(m,2)^int(t,2))[2:].rjust(math.ceil(klen/4),'0')
+    temp=hex(int(x2+m+y2,2))[2:].encode()#转为字节串
+    C3=sm3.sm3_hash(func.bytes_to_list(temp))
+    return C1,C2,C3
+
+
+
+def decrypt(dB,c,a,b,p):
+    '''解密'''
+    #首先提取c1,c2,c3
+    plen=len(hex(p)[2:])
+    c1=c[:(2*plen+2)][2:]#并去掉'04'
+    c2=c[(2*plen+2):-64]
+    c3=c[-64:]
+    klen=(len(c)-2-plen*2-64)*4
+    #验证C1是否满足曲线方程y2=x3+ax+b
+    x1=int(c1[:plen],16)
+    y1=int(c1[plen:],16)
+    if pow(y1,2,p)!=(pow(x1,3,p)+a*x1+b)%p:
+        print('aa')
+        return False
+    #计算x2,y2
+    x2,y2=multipoint(x1,y1,dB,a,p)
+    x2='{:0256b}'.format(x2)
+    y2='{:0256b}'.format(y2)
+    #计算t
+    t=KDF(x2+y2,klen)
+    if t==0:
+        print('aa')
+        return False
+    #计算M'
+    m=bin(int(c2,16)^int(t,2))[2:].rjust(klen,'0')
+    #验证hash
+    temp=hex(int(x2+m+y2,2))[2:].encode()#转为字节串
+    u=sm3.sm3_hash(func.bytes_to_list(temp))
+    if u!=c3:
+        print('ab')
+        return False
+    m=hex(int(m,2))[2:]
+    m=str(bytes.fromhex(m))
+    return m
+    
+
+
+    
+
+#第一步,产生私钥
+dB=randint(1,n-1)
+#第二步，计算公钥
+xB,yB=multipoint(GX,GY,dB,a,p)
+
+print('------------加密------------')
+plaintext='zhelishimingwen'
+print("明文:",plaintext)
+c1,c2,c3=encrypt(dB,xB,yB,plaintext)
+c=c1+c2+c3
+print("密文:",c)
+
+print('------------解密------------')
+pt=decrypt(dB,c,a,b,p)
+if pt:
+    print("解密结果:",pt)
+
+
